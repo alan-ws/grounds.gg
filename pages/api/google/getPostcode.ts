ddb27a0ad7e15222558288163bf43b2580848e35
Alan Williams <alan.ws.private@gmail.com>
2021-12-16 13:38:41 +0000

    wip: postcode with interface and filter

--- pages/api/google/getPostcode.ts
+++ pages/api/google/getPostcode.ts
@@ -1,5 +1,17 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
+type AddressComponent = {
+  'long_name': string;
+  'short_name': string;
+  'types': string[]
+}
+
+type Result = {'address_components': AddressComponent[]};
+
+interface IResults {
+  results: Result[]
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
@@ -8,19 +20,17 @@ export default async function handler(
   const key = process.env.GOOGLE_API_KEY;
   const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${key}`;
   const response = await fetch(url);
-  const data: { results: any[] } = await response.json();
+  const data: IResults = await response.json();
 
-  const postcode = data.results.filter((address: any) => {
-    console.log(address.types);
-    if (address.types.includes("postal_code")) {
-      return address.address_components.filter((component: any) => {
-        console.log(component);
-        if (component.types.includes("postal_code_prefix")) {
-          return component.long_name;
-        }
-      });
-    }
-  });
+  const postcode = data.results.filter((result: Result) => {
+    return result.address_components.filter((address: AddressComponent) => {
+      if (address.types.includes('postal_code_prefix'))
+        return address.long_name;
+
+      if (address.types.includes('postal_code'))
+        return address.long_name;
+    })
+  })
 
   if (postcode.length < 1)
     res.status(404).json({
@@ -28,6 +38,6 @@ export default async function handler(
     });
 
   res.status(200).json({
-    postcode,
+    postcode: postcode[0],
   });
 }
